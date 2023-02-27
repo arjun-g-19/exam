4ec6b6a1cea606bd4ce986168bf78130e73bf812
Arjun Gaikwad <arjun.gkwd19@gmail.com>
2023-02-27 11:59:03 +0530

    My Updated commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 class HelloWorld{
     public static void main(String[] args) {
         System.out.println("Hello from the second trial of Docker and git");
+        System.out.println("This is my second commit");
     }
 }
